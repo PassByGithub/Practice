d472e27c5fa2ebbaf164f50f4400eaa6106b4d8e
PassByGithub <31396300+PassByGithub@users.noreply.github.com>
2017-08-30 17:56:26 +0800

    Try except usage and os.exit/sys.exit usage

---
+++ Mega_Project_List/Classic_Algorithm/Collatz_Conjecture.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+def reach(number):
+    counter=0
+    if number<=1:
+        try:sys.exit(0)
+        except:
+            print("the number is not true")
+    while number!=1:
+        if number%2==0:number//=2
+        else:number=number*3+1
+        counter=counter+1
+    return counter
+
+if __name__=='__main__':
+    number=input("Enter a number which is bigger than 1")
+    try:int(number)
+    except:
+        print("The number is not a integer")
+        os._exit(0)
+    number=int(number)
+    print(reach(number))
